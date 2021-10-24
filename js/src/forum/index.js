df30cfbc30ee79d0f19b4db48b90822df6034d45
bumbutudor <bumbutudor10@gmail.com>
2021-10-24 10:43:44 +0300

    remove item-tags

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,6 +1,6 @@
 import moveButtonToRight from './settings/moveButtonToRight.js';
 import dispatchSortItems from './settings/dispatchSortItems.js';
-import CardItem from './settings/CardItem.js';
+// import CardItem from './settings/CardItem.js';
 
 app.initializers.add('tudor/economia-ui', () => {
   moveButtonToRight();
